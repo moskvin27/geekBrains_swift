b779c2d04753fa25795eac4b42ff9c191fbd7c78
Stanislav <moskvin27@gmail.com>
2021-08-01 17:01:51 +0300

    ver. 2

--- SwiftBasics/lsSeven/7.1Ls_TrubekoStanislav .playground/Contents.swift
+++ SwiftBasics/lsSeven/7.1Ls_TrubekoStanislav .playground/Contents.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-// 1. Придумать класс, методы которого могут завершаться неудачей и возвращать либо значение, либо ошибку Error?. Реализовать их вызов и обработать результат метода при помощи конструкции if let, или guard let.
+/*
+1. Придумать класс, методы которого могут завершаться неудачей и возвращать либо значение, либо ошибку Error?. Реализовать их вызов и обработать результат метода при помощи конструкции if let, или guard let.
+2. Придумать класс, методы которого могут выбрасывать ошибки. Реализуйте несколько throws-функций. Вызовите их и обработайте результат вызова при помощи конструкции try/catch.
+*/
 
 struct Longboard {
     var style: String
@@ -10,10 +13,10 @@ struct Longboard {
 }
 
 enum Defective: Error {
-    case noBord                  // доска отсутсвует
-    case brokenBearings          // разбитые подшипники
-    case sizeError               // превышение длины деки
-    case speedError              // слишком маленькая скорость
+    case noBord
+    case brokenBearings
+    case sizeError
+    case speedError
 }
 
 class ShowCase {
@@ -22,19 +25,19 @@ class ShowCase {
         "carvingBoard": Longboard(style: "carving", bearning: false, size: 100, speed: 50),
         "cruisingBoard": Longboard(style: "cruising", bearning: true, size: 150, speed: 40),
         "downhillBoard": Longboard(style: "downhill", bearning: true, size: 110, speed: 80),
-        "dancngBoard": Longboard(style: "dancng", bearning: true, size: 110, speed: 30)
+        "dancngBoard": Longboard(style: "dancing", bearning: true, size: 110, speed: 30)
     ]
     
     var maxSizeBoard = 120
     var minSpeedBoard = 40
     
-    func takeBoard (itemNamed name: String) throws {
+    func takeBoard (itemNamed name: String) throws  {
         
         guard let boardCase = board[name] else {
             throw Defective.noBord
         }
         
-        guard boardCase.bearning == true else {
+        guard boardCase.bearning else {
             throw Defective.brokenBearings
         }
         
@@ -45,12 +48,59 @@ class ShowCase {
         guard boardCase.speed > minSpeedBoard else {
             throw Defective.speedError
         }
-        
+        print(name)
     }
 }
 
-print(name)
+var result = ShowCase()
 
+do {
+    try result.takeBoard(itemNamed: "carvingBoard")
+} catch Defective.noBord {
+    print("Текущей позиции нет")
+} catch Defective.brokenBearings {
+    print("Подшипники разбиты")
+} catch Defective.sizeError {
+    print("Превышение длины деки")
+} catch Defective.speedError {
+    print("Развиваемая скорость недостаточна")
+}
+
+do {
+    try result.takeBoard(itemNamed: "cruisingBoard")
+} catch Defective.noBord {
+    print("Текущей позиции нет")
+} catch Defective.brokenBearings {
+    print("Подшипники разбиты")
+} catch Defective.sizeError {
+    print("Превышение длины деки")
+} catch Defective.speedError {
+    print("Развиваемая скорость недостаточна")
+}
+
+do {
+    try result.takeBoard(itemNamed: "downhillBoard")
+} catch Defective.noBord {
+    print("Текущей позиции нет")
+} catch Defective.brokenBearings {
+    print("Подшипники разбиты")
+} catch Defective.sizeError {
+    print("Превышение длины деки")
+} catch Defective.speedError {
+    print("Развиваемая скорость недостаточна")
+}
+
+do {
+    try result.takeBoard(itemNamed: "dancing")
+} catch Defective.noBord {
+    print("Текущей позиции нет")
+} catch Defective.brokenBearings {
+    print("Подшипники разбиты")
+} catch Defective.sizeError {
+    print("Превышение длины деки")
+} catch Defective.speedError {
+    print("Развиваемая скорость недостаточна")
+}
 
 
 
